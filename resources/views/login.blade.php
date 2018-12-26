e87bfe2eae6da0a62d2ab4ae3d2e95cd10c41c48
Dave Medrano <evadonardem@gmail.com>
2018-12-26 02:52:14 +0000

    Added Admin LTE assets

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ASCT Login</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
+    </head>
+    <body class="hold-transition login-page">
+        <div class="login-box">
+          <div class="login-logo">
+            <a href="#"><b>ASCT</b> School Forms</a>
+          </div>
+          <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            <form class="" action="index.html" method="post">
+              <div class="form-group has-feedback">
+                <input type="email" class="form-control" placeholder="Email">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+              </div>
+              <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+              </div>
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </form>
+            <br>
+            <a href="#">I forgot my password</a><br>
+            <a href="register.html" class="text-center">Register a new membership</a>
+          </div>
+        </div>
+    </body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+   // .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles([
+  'vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css',
+  'vendor/almasaeed2010/adminlte/bower_components/font-awesome/css/font-awesome.min.css',
+  'vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css',
+], 'public/css/app.css');
+
+mix.scripts([
+  'vendor/almasaeed2010/adminlte/bower_components/jquery/dist/jquery.min.js',
+  'vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js',
+  'vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js',
+], 'public/js/app.js');
+
+mix.copyDirectory('vendor/almasaeed2010/adminlte/bower_components/bootstrap/fonts', 'public/fonts');
+mix.copyDirectory('vendor/almasaeed2010/adminlte/bower_components/font-awesome/fonts', 'public/fonts');
